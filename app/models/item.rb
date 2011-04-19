1ea31f428066cff20571c87dc94bd4727717a0df
David Barker <kzar@kzar.co.uk>
2011-04-19 20:11:04 +0100

    Give correct URLs in feed for internal use.

--- app/models/item.rb
+++ app/models/item.rb
@@ -27,15 +27,15 @@ class Item < ActiveRecord::Base
     '/FileShare/index.cfm?' + query.to_query
   end
 
-  def server(ip)
-    if ip and APP_CONFIG['internal_ips'].include? ip
+  def server(ip, internal)
+    if internal or (ip and APP_CONFIG['internal_ips'].include? ip)
       APP_CONFIG['rincex_server_internal']
     else
       APP_CONFIG['rincex_server']
     end
   end
 
-  def url(ip=nil)
-    'https://' + server(ip) + path
+  def url(ip=nil, internal=false)
+    'https://' + server(ip, internal) + path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Scraper::Application.routes.draw do
   get "/" => "items#index"
   get "/feed-#{Item.first.id}.atom" => "items#feed", :format => 'atom'
+  get "/feed-#{Item.first.id}-:internal.atom" => "items#feed", :format => 'atom', :internal => /internal/
 end
